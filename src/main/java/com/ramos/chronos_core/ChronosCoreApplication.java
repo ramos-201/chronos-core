ab85f2121f94ce04b244f5ddab4e414445c4b66c
Ramos <andfera201@gmail.com>
2025-06-27 10:37:14 -0500

    Initial commit

---
+++ src/main/java/com/ramos/chronos_core/ChronosCoreApplication.java
@@ -0,0 +1,13 @@
+package com.ramos.chronos_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronosCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronosCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramos/chronos_core/ChronosCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramos.chronos_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronosCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
